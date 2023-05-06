18c788288f7e382845f35657e626abf436c6d1ee
emtoldme <506655946@qq.com>
2023-05-06 22:34:24 +0800

    all fixed up

--- src/Dog.java
+++ src/Dog.java
@@ -1,3 +1,19 @@
 public class Dog extends Animal {
+private String sex;//represent sexual appearance.
+    void setBasic(String type,String name,int age,String sex){
+        setAge(age);
+        setType(type);
+        setName(name);
+        this.sex=sex;
+    }
+    public String getSex() {
+        return sex;
+    }
 
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+    void sleep(){
+
+    };
 }
